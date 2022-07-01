60690401f2e9d664cedae7163b37e23d95d96376
Valmyr Tavares Malta de Lima <vlima@fcc.org.br>
2022-07-01 11:36:37 -0300

    Entrando css indicando em qual o container será adicionado o item e ao sair do container a borda azul é retirada e criado um gerador de id para tornar os itens independentes

--- js/module/dragAndDrop.js
+++ js/module/dragAndDrop.js
@@ -14,31 +14,47 @@ export default class dragAndDrop{
             item.addEventListener("dragover", ()=>{
                 this.allowDrop(event)
             })
+            item.addEventListener("dragleave", ()=>{
+                this.OutContent(event)
+            })
         })
         
         this.moveItem.forEach(item => {         
             item.setAttribute("draggable", true)
-            item.setAttribute("id","xuxu")
+            item.setAttribute("id", this.geraString(6))
 
             item.addEventListener("dragstart",(event)=>{
                 this.dragStart(event)
             })
         })
     }
-     drop(event){
-        event.preventDefault()
+    drop(event){
+        event.preventDefault()       
+        event.target.classList.remove("bord");     
         var data = event.dataTransfer.getData("Text");
         event.target.appendChild(document.getElementById(data));            
     }
     allowDrop(event) {
         event.preventDefault();
+        event.target.classList.add("bord");
     }
 
-    dragStart(event){
+    dragStart(event){        
         event.dataTransfer.setData("Text", event.target.id);
+    }
+     geraString(tamanho){
+        var StringAleatoria = "";
+        var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for(var i=0; i<tamanho; i++){
+            StringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+        }
+        return StringAleatoria           
+    }
+    OutContent(event){
+        event.target.classList.remove("bord");   
     }
     init(){       
         this.addEvent();
-     }
+    }
 }
 
